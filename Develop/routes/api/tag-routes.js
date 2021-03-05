0123866f9ae9b2b6726a4634fbae572ca421a2aa
Nina Jade <jaderiver64@gmail.com>
2021-03-05 13:53:51 -0600

    Model updates

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,7 +7,9 @@ router.get('/', (req, res) => {
     // find all tags
     Tag.findAll({
         include: [Product]
-    }).then(tagData => { res.json(tagData) });
+    }).then(tagData => {
+        res.json(tagData);
+    });
     // be sure to include its associated Product data
 });
 
@@ -18,20 +20,31 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id
         }
-    }).then(tagData => { res.json(tagData) });
+    }).then(tagData => { res.json(tagData); });
     // be sure to include its associated Product data
 });
 
 router.post('/', (req, res) => {
     // create a new tag
+    Tag.create(req.body).then(tagData => {
+        res.json(tagData);
+    });
 });
 
 router.put('/:id', (req, res) => {
     // update a tag's name by its `id` value
+    Tag.update(req.body, {
+        where: {
+            id: req.body.id
+        }
+    }).then(tagData => { res.json(tagData); });
 });
 
 router.delete('/:id', (req, res) => {
     // delete on tag by its `id` value
+    Tag.destroy({
+        where: { id: req.params.id }
+    }).then(tagData => { res.json(tagData); });
 });
 
 module.exports = router;
