e51a12a4ba19e18157c2f38266640109654e4330
Nina Jade <jaderiver64@gmail.com>
2021-03-05 23:41:17 -0600

    testing routes and fixing errors

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
             where: {
                 id: req.params.id
             }
-        }).then(tagData => { res.json(tagData); })
+        }).then(tagData => res.json(tagData))
         .catch(err => {
             console.log(err);
             res.status(404).json(err);
@@ -40,12 +40,11 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     // create a new tag
-    Tag.create(req.body).then(tagData => {
-        res.json(tagData);
-    }).catch(err => {
-        console.log(err);
-        res.status(404).json(err);
-    });
+    Tag.create(req.body).then(tagData => res.json(tagData))
+        .catch(err => {
+            console.log(err);
+            res.status(404).json(err);
+        });
 });
 
 router.put('/:id', (req, res) => {
@@ -54,7 +53,7 @@ router.put('/:id', (req, res) => {
             where: {
                 id: req.params.id
             }
-        }).then(tagData => { res.json(tagData); })
+        }).then(tagData => res.json(tagData))
         .catch(err => {
             console.log(err);
             res.status(404).json(err);
@@ -65,7 +64,7 @@ router.delete('/:id', (req, res) => {
     // delete on tag by its `id` value
     Tag.destroy({
             where: { id: req.params.id }
-        }).then(tagData => { res.json(tagData); })
+        }).then(tagData => res.json(tagData))
         .catch(err => {
             console.log(err);
             res.status(404).json(err);
